e21dd7a884cccbbfad33db6f0a6c07c545bdbcd6
Anastasia <127711722+damnsesh@users.noreply.github.com>
2024-04-10 12:31:05 +0300

    Add files via upload
    Две новые страницы в разработке

---
+++ academic.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/academic-styles.css">
+      <link rel="stylesheet" href="header/header-style.css">
+      <link rel="stylesheet" href="footer/footer-style.css">
+      <link rel="shortcut icon" href="../images/fpt.ico">
+    <title>Преподавателю</title>
+</head>
+<body>
+<?php include('header/header.html'); ?>
+
+<div class="academic-title wrap">
+    <div class="container-academic-title container">
+    <h1>Преподавателю</h1>
+    </div>
+</div>
+
+<div class="academic-block wrap">
+    <div class="container-academic-block container">
+        <div class="item-academic-block" id="blue">
+            <div class="text-icon">
+            <img src="images/question-svgrepo-com.svg" alt="">
+            <h2>Задай вопрос методисту</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-academic-block" id="pink">
+        <div class="text-icon">
+            <img src="images/office-desk-svgrepo-com.svg" alt="">
+            <h2>Виртуальный методический кабинет</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-academic-block" id="purple">
+        <div class="text-icon">
+            <img src="images/documents-minimalistic-svgrepo-com.svg" alt="">
+            <h2>Методические документы</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-academic-block" id="green">
+        <div class="text-icon">
+            <img src="images/chart-mixed-alt-svgrepo-com.svg" alt="">
+            <h2>Сводный график учебного процесса</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+        <div class="item-academic-block" id="blue">
+        <div class="text-icon">
+            <img src="images/schedule-repeat-child-svgrepo-com.svg" alt="">
+            <h2>График дежурств</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-academic-block" id="pink">
+        <div class="text-icon">
+            <img src="images/teacher-svgrepo-com.svg" alt="">
+            <h2>Наставничество</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-academic-block" id="purple">
+        <div class="text-icon">
+            <img src="images/psychologist-svgrepo-com.svg" alt="">
+            <h2>Социально-психологическая служба</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+
+        <div class="item-academic-block" id="green">
+        <div class="text-icon">
+            <img src="images/lastbox.svg" alt="">
+            <h2>Рекомендации в период дистанционного обучения</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+        
+        <div class="empty">
+        </div>
+    </div>
+</div>
+
+
+<?php include('footer/footer.html'); ?> 
+</body>
+</html>
---
+++ safety.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/safety-styles.css">
+      <link rel="stylesheet" href="header/header-style.css">
+      <link rel="stylesheet" href="footer/footer-style.css">
+      <link rel="shortcut icon" href="../images/fpt.ico">
+    <title>Безопасность</title>
+</head>
+<body>
+<?php include('header/header.html'); ?>
+
+<div class="safety-title wrap">
+    <div class="container-safety-title container">
+    <h1>Безопасность</h1>
+    </div>
+</div>
+
+<div class="safety-block wrap">
+    <div class="container-safety-block container">
+        <div class="item-safety-block" id="blue">
+            <div class="text-icon">
+            <img src="images/security-secure-protection-7-svgrepo-com.svg" alt="">
+            <h2>Антитеррористическая безопасность</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-safety-block" id="pink">
+        <div class="text-icon">
+            <img src="images/fireworks.svg" alt="">
+            <h2>Пожарная безопасность</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-safety-block" id="purple">
+        <div class="text-icon">
+            <img src="images/road-alt-svgrepo-com.svg" alt="">
+            <h2>Безопасность дорожного движения</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-safety-block" id="green">
+        <div class="text-icon">
+            <img src="images/security-secure-protection-8-svgrepo-com.svg" alt="">
+            <h2>Информационная безопасность</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+        <div class="item-safety-block" id="blue">
+        <div class="text-icon">
+            <img src="images/security-secure-protection-14-svgrepo-com.svg" alt="">
+            <h2>Финансовая безопасность</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-safety-block" id="pink">
+        <div class="text-icon">
+            <img src="images/security-secure-protection-27-svgrepo-com.svg" alt="">
+            <h2>Гражданская оборона</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-safety-block" id="purple">
+        <div class="text-icon">
+            <img src="images/security-safe-svgrepo-com.svg" alt="">
+            <h2>Охрана труда и БЖД</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+
+        <div class="item-safety-block" id="green">
+        <div class="text-icon">
+            <img src="images/security-secure-protection-svgrepo-com.svg" alt="">
+            <h2>Защита персональных данных</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-safety-block" id="purple">
+        <div class="text-icon">
+            <img src="images/list-check-svgrepo-com.svg" alt="">
+            <h2>Комплекс безопасности (рекомендации населению)</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+        <div class="item-safety-block" id="purple">
+        <div class="text-icon">
+            <img src="images/bandaid-svgrepo-com.svg" alt="">
+            <h2>Анализ производственного травматизма</h2>
+            </div>
+            <div class="icon-block">
+            <a href="#">подробнее</a>
+            <img src="images/arrow.svg" alt="arrow" class="ic">
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<?php include('footer/footer.html'); ?> 
+</body>
+</html>
